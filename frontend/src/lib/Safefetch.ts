fbb3dc9e29dcec4b7f6b23e3b6135b15b45556da
Mechazzz <129221431+Mechazzz@users.noreply.github.com>
2024-04-29 17:05:43 +0200

    backend/frontend/Readme setup

---
+++ frontend/src/lib/Safefetch.ts
@@ -0,0 +1,58 @@
+import { z } from "zod";
+
+type Response<Type> =
+  | {
+      success: true;
+      status: number;
+      data: Type;
+    }
+  | {
+      success: false;
+      status: number | null;
+    };
+
+type Method = "GET" | "POST" | "PATCH" | "DELETE";
+
+const baseUrl = "http://localhost:3000";
+
+export const safeFetch = async <Schema extends z.ZodTypeAny>(config: {
+  method: Method;
+  path: string;
+  schema: Schema;
+  payload?: unknown;
+}): Promise<Response<z.infer<typeof config.schema>>> => {
+  const { method, path, schema, payload } = config;
+  const token = localStorage.getItem("token");
+  try {
+    const response = await fetch(baseUrl + path, {
+      method,
+      headers: payload
+        ? {
+            "Content-Type": "application/JSON",
+            auth: token || "",
+          }
+        : { auth: token || "" },
+      body: payload ? JSON.stringify(payload) : undefined,
+    });
+
+    if (response.status >= 500)
+      return { success: false, status: response.status };
+
+    if (response.status >= 400)
+      return { success: false, status: response.status };
+
+    const data = await response.json();
+    console.log(data);
+
+    const result = schema.safeParse(data);
+    console.log(result);
+    if (!result.success) {
+      console.log(result.error);
+      return { success: false, status: response.status };
+    }
+
+    return { data: result.data, success: true, status: response.status };
+  } catch (error) {
+    return { success: false, status: null };
+  }
+};
